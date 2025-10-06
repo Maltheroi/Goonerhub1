ccec5cdb4b40a513d3ef118cbf2c7b9a0633f42a
Tim <tim_ketting02@hotmail.com>
2025-10-06 10:12:49 +0200

    yes

---
+++ src/Kat.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class Kat {
+    public class kat {
+        public static void kat() {
+            String kat;
+
+
+            kat = "hunde";
+
+            Random randomantalKatte = new Random();
+            int antalKatte = randomantalKatte.nextInt(100) + 1;
+
+            System.out.println("Der er " + randomantalKatte + " " + kat + " " + "i haven");
+
+
+        }
+
+    }
+}
